1e7170eb917f1e0aa6e30620c8aaf3a736ffb36c
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-08-09 23:11:30 +0800

    fix: update product section titles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
-<div class="h-full flex items-center justify-center gap-16">
-    <div class="flex flex-col items-center gap-4">
-        <span>Beddings Set</span>
-        <a href="/beddings" class="bg-blue-700 text-white px-4 py-2 rounded-full">Shop Collection</a>
-    </div>
-    <div class="flex flex-col items-center gap-4">
-        <span>Loungewear Set</span>
-        <a href="/loungewear" class="bg-blue-700 text-white px-4 py-2 rounded-full">Shop Collection</a>
-    </div>
+<div class="flex h-full items-center justify-center gap-16">
+	<div class="flex flex-col items-center gap-4">
+		<span>Beddings</span>
+		<a href="/beddings" class="rounded-full bg-blue-700 px-4 py-2 text-white">Shop Collection</a>
+	</div>
+	<div class="flex flex-col items-center gap-4">
+		<span>Loungewear</span>
+		<a href="/loungewear" class="rounded-full bg-blue-700 px-4 py-2 text-white">Shop Collection</a>
+	</div>
 </div>
