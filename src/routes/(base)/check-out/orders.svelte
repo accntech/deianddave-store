4dc8ed5672edf3a9775386446daa0d4d394d6849
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-08-14 18:38:20 +0800

    fix(check-out): improve layout

--- src/routes/(base)/check-out/orders.svelte
+++ src/routes/(base)/check-out/orders.svelte
@@ -42,101 +42,108 @@
 	const shop = getShopState();
 </script>
 
-<div class="space-y-4 px-4 py-6">
-	{#each cart.orders as { item, quantity }}
-		{@const { wholeNumber, decimal } = splitNumberToString(item.price * quantity)}
-		<div class="relative flex gap-2 rounded-3xl bg-[#EEEEEE] p-4">
-			<object
-				type="image/png"
-				data={item.image === '' ? PUBLIC_DEFAULT_PRODUCT_IMAGE : item.image}
-				class="size-24 shrink-0 rounded-xl border object-cover"
-				aria-label={item.product.name}
-			>
-				<img
-					src={PUBLIC_DEFAULT_PRODUCT_IMAGE}
-					alt={item.product.name}
-					class="h-full w-full object-cover"
-				/>
-			</object>
-			<div class="flex-1">
-				<span class="text-md font-medium">{item.product.name}</span>
-				<span class="flex flex-nowrap gap-4 text-sm whitespace-pre-wrap text-muted-foreground">
-					{createDescription(item)}
-				</span>
-				<div class="flex items-center text-sm text-muted-foreground">
-					<span>{quantity} {item.size.name}</span>
-					<div
-						style="background-color: {item.color.hexCode}"
-						class="mr-1 ml-4 size-4 shrink-0 rounded-full"
-					></div>
-					<span>{item.color.name}</span>
+{#if cart.orders.length > 0}
+	<div class="space-y-4 px-4 py-6">
+		{#each cart.orders as { item, quantity }}
+			{@const { wholeNumber, decimal } = splitNumberToString(item.price * quantity)}
+			<div class="relative flex gap-2 rounded-3xl bg-[#EEEEEE] p-4">
+				<object
+					type="image/png"
+					data={item.image === '' ? PUBLIC_DEFAULT_PRODUCT_IMAGE : item.image}
+					class="size-24 shrink-0 rounded-xl border object-cover"
+					aria-label={item.product.name}
+				>
+					<img
+						src={PUBLIC_DEFAULT_PRODUCT_IMAGE}
+						alt={item.product.name}
+						class="h-full w-full object-cover"
+					/>
+				</object>
+				<div class="flex-1">
+					<span class="text-md font-medium">{item.product.name}</span>
+					<span class="flex flex-nowrap gap-4 text-sm whitespace-pre-wrap text-muted-foreground">
+						{createDescription(item)}
+					</span>
+					<div class="flex items-center text-sm text-muted-foreground">
+						<span>{quantity} {item.size.name}</span>
+						<div
+							style="background-color: {item.color.hexCode}"
+							class="mr-1 ml-4 size-4 shrink-0 rounded-full"
+						></div>
+						<span>{item.color.name}</span>
+					</div>
+					<div class="mt-2 flex items-baseline gap-2">
+						<span class="text-xs font-medium text-primary">₱</span>
+						<div class="flex gap-1">
+							<span class="text-md font-semibold">{wholeNumber}</span>
+							<span class="mt-1 align-top text-xs font-medium">
+								{decimal}
+							</span>
+						</div>
+					</div>
 				</div>
-				<div class="mt-2 flex items-baseline gap-2">
+				<button
+					type="button"
+					class="absolute top-3 right-3 shrink-0 rounded-full border p-0.5"
+					onclick={() => cart.remove(item.id)}
+					aria-label={`Remove ${item.product.name}`}
+				>
+					<XIcon class="size-4" />
+				</button>
+			</div>
+		{/each}
+	</div>
+{:else}
+	<div class="flex h-32 w-full items-center justify-center">
+		<span class="text-muted-foreground"> Your cart is empty. </span>
+	</div>
+{/if}
+<Button
+	variant="outline"
+	class="mx-4 mb-56 place-self-start rounded-lg shadow-none"
+	onclick={() => goto(shop.lastShop)}
+>
+	<ShoppingCartIcon />
+	Continue shopping
+</Button>
+
+{#if cart.orders.length > 0}
+	<div
+		class="fixed bottom-0 flex w-full flex-col gap-4 rounded-t-4xl border-t bg-muted/75 p-6 backdrop-blur-sm"
+	>
+		<div class="flex flex-col">
+			<span class="-mb-1 text-xs">Total Amount</span>
+			{#if totalAmount > 0}
+				{@const { wholeNumber, decimal } = splitNumberToString(totalAmount)}
+				<div class="flex items-baseline gap-2">
 					<span class="text-xs font-medium text-primary">₱</span>
 					<div class="flex gap-1">
-						<span class="text-md font-semibold">{wholeNumber}</span>
+						<span class="text-xl font-semibold">{wholeNumber}</span>
 						<span class="mt-1 align-top text-xs font-medium">
 							{decimal}
 						</span>
 					</div>
 				</div>
-			</div>
-			<button
-				type="button"
-				class="absolute top-3 right-3 shrink-0 rounded-full bg-red-500 p-0.5 text-xs font-medium text-primary-foreground transition-all outline-none hover:bg-red-600 focus:ring-3 focus:ring-red-500/50"
-				onclick={() => cart.remove(item.id)}
-				aria-label={`Remove ${item.product.name}`}
-			>
-				<XIcon class="size-4" />
-			</button>
+			{:else}
+				<span class="text-sm text-muted-foreground">₱ 0.00</span>
+			{/if}
 		</div>
-	{/each}
-
-	<Button
-		variant="outline"
-		class="mb-56 place-self-start rounded-lg shadow-none"
-		onclick={() => goto(shop.lastShop)}
-	>
-		<ShoppingCartIcon />
-		Continue shopping
-	</Button>
-</div>
-
-<div
-	class="fixed bottom-0 flex w-full flex-col gap-4 rounded-t-4xl border-t bg-muted/75 p-6 backdrop-blur-sm"
->
-	<div class="flex flex-col">
-		<span class="-mb-1 text-xs">Total Amount</span>
-		{#if totalAmount > 0}
-			{@const { wholeNumber, decimal } = splitNumberToString(totalAmount)}
-			<div class="flex items-baseline gap-2">
-				<span class="text-xs font-medium text-primary">₱</span>
-				<div class="flex gap-1">
-					<span class="text-xl font-semibold">{wholeNumber}</span>
-					<span class="mt-1 align-top text-xs font-medium">
-						{decimal}
-					</span>
-				</div>
+		<button
+			onclick={checkout}
+			disabled={cart.orders.length === 0}
+			class="w-full rounded-xl bg-primary px-6 py-3 text-center text-sm font-medium text-primary-foreground transition-all duration-300 outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 dark:hover:bg-input/50"
+		>
+			Checkout Now
+		</button>
+		<div class="space-y-1">
+			<p class="text-sm font-medium">You can pay thru the following</p>
+			<div class="flex flex-wrap gap-2">
+				<img class="w-12 rounded-md border object-cover" src={MasterCard} alt="Mastercard" />
+				<img class="w-12 rounded-md border object-cover" src={Visa} alt="Visa" />
+				<img class="w-12 rounded-md border object-cover" src={Gcash} alt="Gcash" />
+				<img class="w-12 rounded-md border object-cover" src={PayMaya} alt="PayMaya" />
+				<img class="w-12 rounded-md border object-cover" src={GrabPay} alt="GrabPay" />
 			</div>
-		{:else}
-			<span class="text-sm text-muted-foreground">₱ 0.00</span>
-		{/if}
-	</div>
-	<button
-		onclick={checkout}
-		disabled={cart.orders.length === 0}
-		class="w-full rounded-xl bg-primary px-6 py-3 text-center text-sm font-medium text-primary-foreground transition-all duration-300 outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 dark:hover:bg-input/50"
-	>
-		Checkout Now
-	</button>
-	<div class="space-y-1">
-		<p class="text-sm font-medium">You can pay thru the following</p>
-		<div class="flex flex-wrap gap-2">
-			<img class="w-12 rounded-md border object-cover" src={MasterCard} alt="Mastercard" />
-			<img class="w-12 rounded-md border object-cover" src={Visa} alt="Visa" />
-			<img class="w-12 rounded-md border object-cover" src={Gcash} alt="Gcash" />
-			<img class="w-12 rounded-md border object-cover" src={PayMaya} alt="PayMaya" />
-			<img class="w-12 rounded-md border object-cover" src={GrabPay} alt="GrabPay" />
 		</div>
 	</div>
-</div>
+{/if}
