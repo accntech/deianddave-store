9fc391161a1ddebfa5db3b050989d4fe83631c1d
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-08-19 21:43:52 +0800

    fix(discount-helper): handle undefined discount parameter

--- src/lib/utils/discount-helper.ts
+++ src/lib/utils/discount-helper.ts
@@ -1,6 +1,8 @@
 import type { Discount } from '$lib/services';
 
-export const computeDiscount = (total: number, discount: Discount) => {
+export const computeDiscount = (total: number, discount: Discount | undefined) => {
+	if (!discount) return 0;
+
 	if (discount.type === 'percentage') {
 		return (total * discount.value) / 100;
 	} else if (discount.type === 'fixed') {
