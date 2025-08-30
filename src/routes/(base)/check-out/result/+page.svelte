576a238812bb47bc89da3d9635a188db27ef7e69
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-08-30 21:33:16 +0800

    fix: improve checkout result content

--- src/routes/(base)/check-out/result/+page.svelte
+++ src/routes/(base)/check-out/result/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/state';
 	import { getCartState } from '$lib/client/cart.svelte';
+	import { MailCheckIcon, MailIcon, SendIcon } from '@lucide/svelte';
 	import { onMount } from 'svelte';
 
 	let { data } = $props();
@@ -14,18 +15,55 @@
 			cart.orders = [];
 		}
 	});
+
+	const emailString =
+		"An email receipt including the details about your order has been sent to the email address you provided. If you didn't receive it, please check your spam folder.\n\nFor further assistance, please contact our support team.";
 </script>
 
-{#if data.id}
-	<div class="flex flex-col items-center justify-center">
+<div class="flex h-[calc(100dvh-4rem)] flex-col items-center justify-center">
+	{#if data.id}
 		{#if success}
-			<h1>Payment is properly processed</h1>
+			{@render successContent()}
 		{:else}
-			<h1>Payment is not processed. Please try again!</h1>
+			{@render failedContent()}
 		{/if}
+	{:else}
+		{@render noInfoContent()}
+	{/if}
+</div>
+
+{#snippet successContent()}
+	<div class="mx-8 flex flex-col items-center justify-center">
+		<h1 class="text-[clamp(1.5rem,calc(5vw+1rem),4rem)] font-semibold text-primary">Thank you!</h1>
+		<h2 class="text-[clamp(0.875rem,calc(3vw),2rem)]">
+			Your order has been successfully processed.
+		</h2>
+
+		<div class="mt-24 flex max-w-150 flex-col items-center gap-4 sm:flex-row sm:items-start">
+			<MailCheckIcon class="size-12 shrink-0 text-muted-foreground" />
+			<p class="text-sm text-pretty whitespace-pre-wrap">{emailString}</p>
+		</div>
 	</div>
-{:else}
-	<div class="flex flex-col items-center justify-center">
-		<h1>No payment information available</h1>
+{/snippet}
+
+{#snippet failedContent()}
+	<div class="mx-8 flex flex-col items-center justify-center">
+		<h1 class="text-[clamp(1.5rem,calc(5vw+1rem),4rem)] font-semibold text-secondary">Oops!</h1>
+		<h2 class="text-center text-[clamp(0.875rem,calc(3vw),2rem)]">
+			<p>We're unable to process your order at this time.</p>
+			<p>Please try again!</p>
+		</h2>
 	</div>
-{/if}
+{/snippet}
+
+{#snippet noInfoContent()}
+	<div class="mx-8 flex flex-col items-center justify-center">
+		<h1 class="text-[clamp(1.5rem,calc(5vw+1rem),4rem)] font-semibold text-accent-foreground">
+			Sorry!
+		</h1>
+		<h2 class="text-center text-[clamp(0.875rem,calc(3vw),2rem)]">
+			<p>No payment information available.</p>
+			<p>Make sure you properly filled out the checkout form.</p>
+		</h2>
+	</div>
+{/snippet}
